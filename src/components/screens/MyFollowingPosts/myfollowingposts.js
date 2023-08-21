8ce68abfaeb5076f0065816aa5ebeb49f02f2a65
Kamran-Khan <kamran.khan_ext@moxey.ai>
2023-08-21 08:02:39 +0530

    my following component  UI changes and code refactoring

--- src/components/screens/MyFollowingPosts/myfollowingposts.js
+++ src/components/screens/MyFollowingPosts/myfollowingposts.js
@@ -1,55 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import axios from '../../../helpers/axios';
-import Post from '../post/post';
-import { Alert, AlertTitle } from '@material-ui/lab';
-import PostShimmer from '../../utilityComponents/shimmer';
+import React, { useState, useEffect } from "react";
+import axios from "../../../helpers/axios";
+import Post from "../post/post";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import PostShimmer from "../../utilityComponents/shimmer";
+
+import { Empty, Card } from "antd";
+import "./myfollowing-posts.scss";
 
 const MyFollowingPosts = (props) => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetchAllPosts();
-    }, [])
+  useEffect(() => {
+    fetchAllPosts();
+  }, []);
 
-    //function to fetch My following posts
-    const fetchAllPosts = () => {
-        axios.get('/myfollowingposts', {
-            headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('jwt')
-            }
+  //function to fetch My following posts
+  const fetchAllPosts = () => {
+    axios
+      .get("/myfollowingposts", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("jwt"),
+        },
+      })
+      .then((res) => {
+        console.log(typeof res.data, res.data);
+        if (res.data.posts !== "undefined") {
+          setData(res.data.posts);
+        }
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  return (
+    <div className='my-following-posts-container'>
+      {data && data.length == 0 && <PostShimmer />}
+
+      {!data ? (
+        <Card title='My Following Posts' bordered={true} className='card-block'>
+          <Empty description='No posts available' />
+        </Card>
+      ) : (
+        data.map((item) => {
+          return <Post key={item._id} item={item} />;
         })
-            .then(res => {
-                console.log(typeof (res.data), res.data)
-                if (res.data.posts !== 'undefined') {
-                    setData(res.data.posts)
-                }
-
-            })
-            .catch(err => {
-                console.log(err.response)
-            })
-    }
-
-    return (
-        <div className="home">
-            {
-                data && data.length == 0 && (<PostShimmer />)
-            }
-
-            { !data ?
-                (
-                    <Alert severity="info">
-                        <AlertTitle>Posts</AlertTitle>
-                      No posts available — <strong>Maybe due to your following have not posted anything</strong>
-                    </Alert>
-                )
-                :
-                data.map((item) => {
-                    return <Post key={item._id} item={item} />
-                })
-            }
-        </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default MyFollowingPosts;
